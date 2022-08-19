2522e73e434663b755bab70ef1a513f7ce35c174
demarchenac <demarchenac@uninorte.edu.co>
2022-08-18 21:10:41 -0500

    Magic!

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
 # import libraries
+from multiprocessing import current_process
 import os
 import re
-import json
+import math
 import shutil
 import itertools
 from pathlib import Path
 from typing import Dict, Literal, List, Union
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+
+# import json
+# import pyperclip as pc
 
 
 def folder_setup() -> str:
@@ -98,13 +102,84 @@ def get_options_catalog(codes: List[str]) -> List[SKU]:
             for value in option['values']:
                 value['alias'] = f'Option{index + 1} Value'
         sku['option_length'] = len(sku['options'])
+        sku['replacements'] = []
 
     return skus
 
 
-def fill_basic_rate(filename: str, skus: List[SKU]) -> List[SKU]:
+def cast_as_int_if_possible(value):
+    try:
+        parsed = int(value)
+        return parsed
+    except ValueError:
+        if value == np.NaN:
+            return ''
+        if (type(value) == str):
+            return value.strip()
+        return value.astype(str).replace('nan', '').replace('NaN', '')
+
+
+def fill_sku_data_from_original(filename: str, skus: List[SKU]) -> List[SKU]:
+    search_col = "Variant SKU"                              # O,
+    source_cols = [
+        "Handle",                                           # A,
+        "Title",                                            # B,
+        "Body (HTML)",                                      # C,
+        "Vendor",                                           # D,
+        "Standardized Product Type",                        # E,
+        "Custom Product Type",                              # F,
+        "Tags",                                             # G,
+        "Published",                                        # H,
+        "Variant Grams",                                    # P?,
+        "Variant Inventory Tracker",                        # Q?,
+        "Variant Inventory Policy",                         # S?,
+        "Variant Fulfillment Service",                      # T?,
+        "Variant Requires Shipping",                        # W?,
+        "Variant Taxable",                                  # X?,
+        "Variant Barcode",                                  # Y?,
+        # "Image Alt Text",                                   # AB?,
+        "Gift Card",                                        # AC?,
+        "SEO Title",                                        # AD?,
+        "SEO Description",                                  # AE?,
+        "Google Shopping / Google Product Category",        # AF?,
+        "Google Shopping / Gender",                         # AG?,
+        "Google Shopping / Age Group",                      # AH?,
+        "Google Shopping / MPN",                            # AI?,
+        "Google Shopping / AdWords Grouping",               # AJ?,
+        "Google Shopping / AdWords Labels",                 # AK?,
+        "Google Shopping / Condition",                      # AL?,
+        "Google Shopping / Custom Product",                 # AM?,
+        "Google Shopping / Custom Label 0",                 # AN?,
+        "Google Shopping / Custom Label 1",                 # AO?,
+        "Google Shopping / Custom Label 2",                 # AP?,
+        "Google Shopping / Custom Label 3",                 # AQ?,
+        "Google Shopping / Custom Label 4",                 # AR?,
+        "Variant Image",                                    # AS?,
+        "Variant Weight Unit",                              # AT?,
+        "Variant Tax Code",                                 # AU?,
+        "Cost per item",                                    # AV?,
+        "Status",                                           # AW?,
+    ]
+
     df = pd.read_csv(f'input/{filename}.csv', encoding='utf-8', sep=',')
-    pass
+
+    for sku in skus:
+        search_condition = (df[search_col] == f'{sku["code"]}-01')
+        original_raw = df[search_condition].iloc[0]
+
+        sku['basic_rate'] = int(original_raw['Variant Price'])
+        sku['compare_rate'] = int(original_raw['Variant Compare At Price'])
+
+        for source_col in source_cols:
+
+            formatted_value = cast_as_int_if_possible(original_raw[source_col])
+
+            sku["replacements"].append({
+                "column": source_col,
+                "value": formatted_value
+            })
+
+    return skus
 
 
 def get_output_columns(filename: str, skus: List[SKU]) -> List[str]:
@@ -122,7 +197,7 @@ def get_output_columns(filename: str, skus: List[SKU]) -> List[str]:
 
     cols[old_index[0]: old_index[-1]] = option_cols
 
-    return cols
+    return (cols, option_cols)
 
 
 def format_options_to_dict(option):
@@ -132,17 +207,19 @@ def format_options_to_dict(option):
     }
 
 
-def build_output(columns: List[str], skus: List[SKU]):
-    output_df = pd.DataFrame(columns=columns)
-
-    ram = {}
+def build_output(columns: List[str], skus: List[SKU], option_columns: List[str]):
     separator = '|||'
-    for sku in skus:
-        ram['Variant SKU'] = sku['code']
-        merged = [[f'{option["name"]}{separator}{value["value"]}' for value in option["values"]]
-                  for option in sku['options']]
 
-        combinations = itertools.product(*merged)
+    rows = []
+    for sku in skus:
+        print(f'Processing {sku["code"]}:')
+
+        merged = [[f'{option["name"]}{separator}{value["value"]}'
+                   for value in option["values"]]
+                  for option in sku['options']
+                  ]
+
+        combinations = list(map(list, itertools.product(*merged)))
 
         col_map = {option["name"]: option["alias"]
                    for option in sku['options']}
@@ -150,28 +227,55 @@ def build_output(columns: List[str], skus: List[SKU]):
                                      for value in option["values"]}
                     for option in sku['options']}
 
-        print(json.dumps({"col_map": col_map}))
-        # print(json.dumps({"sku_map": sku_map}))
-        print('---')
+        combination_size = len(combinations)
+        ratio = math.floor(1000 / combination_size)
+        reminder = 1000 - (ratio * len(combinations))
 
-        for combiation in combinations:
-            row_payload = {"rate": 0}
+        should_log = 0
+        for (index, combiation) in enumerate(combinations):
+            combination_number = f'{index +1}'.rjust(2, '0')
+
+            current_progress = (index + 1) / combination_size
+            if (should_log < current_progress):
+                print(
+                    f'  - {(index + 1)}/{combination_size} ({current_progress *100}%)')
+                should_log += 0.25
+
+            row_payload = {}
+            row_payload["Variant SKU"] = f'{sku["code"]}-{combination_number}'
+
+            extra_rate = 0
             for item in combiation:
                 opt_key, opt_val = item.split(separator)
                 col_name = col_map[opt_key]
                 col_value = col_map[opt_key].replace('Name', 'Value')
                 row_payload[col_name] = opt_key
                 row_payload[col_value] = opt_val
-                row_payload["rate"] += rate_map[opt_key][opt_val]
-            print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-            print(json.dumps({"row_payload": row_payload}))
+                extra_rate += rate_map[opt_key][opt_val]
 
-    # for option in sku['options']:
+            for option_column in option_columns:
+                if option_column not in row_payload:
+                    row_payload[option_column] = ''
 
-    #     ram[option['alias']] = sku['code']
-    #     for value in option['values']:
-    #         ram[value['alias']] = value['value']
-    #         print(ram)
+            row_payload['Variant Inventory Qty'] = ratio
+            if (index < reminder):
+                row_payload['Variant Inventory Qty'] += 1
+
+            row_payload['Variant Price'] = sku['basic_rate'] + extra_rate
+            row_payload['Variant Compare At Price'] = sku['compare_rate'] + extra_rate
+
+            for replacement in sku["replacements"]:
+                row_payload[replacement["column"]] = replacement["value"]
+
+            for missing_column in columns:
+                if missing_column not in row_payload:
+                    row_payload[missing_column] = ''
+
+            rows.append(row_payload)
+
+    output_df = pd.DataFrame.from_dict(rows)
+    output_df = output_df.reindex(columns=columns)
+    return output_df
 
 
 def main():
@@ -181,12 +285,19 @@ def main():
 
     codes = get_codes_from_original(filename=filename)
     skus = get_options_catalog(codes=codes)
-    output_columns = get_output_columns(filename=filename, skus=skus)
-    # print(output_columns)
-    build_output(columns=output_columns, skus=skus)
-    # print(json.dumps({"skus": skus}))
-    # skus = fill_basic_rate(filename, options)
-    pass
+    skus = fill_sku_data_from_original(filename=filename, skus=skus)
+    output_columns, option_columns = get_output_columns(filename=filename,
+                                                        skus=skus)
+
+    output_df = build_output(columns=output_columns,
+                             skus=skus,
+                             option_columns=option_columns)
+
+    output_df.to_csv(f'{output}/{filename}_from_catalog.csv',
+                     index=False, sep=",", encoding='utf-8')
+    print("CSV Generated!")
+
+    return 0
 
 
 if __name__ == '__main__':
